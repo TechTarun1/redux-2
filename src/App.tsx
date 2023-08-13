47c886fc7b0724bed432ddd511d21e9e4a5effff
Tarun <Tarun.Boddeboyina@wallero.com>
2023-08-13 13:00:56 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import ProductList from "./components/ProductList";
+import { useState } from "react";
+import CartContextProvider from "./context/cart";
+import { Provider } from "react-redux";
+import store from "./store";
 
-function App() {
+export default function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const iphone = {
+    id: 1,
+    title: "Apple iPhone 13",
+    price: "79,999",
+    image: "https://m.media-amazon.com/images/I/61l9ppRIiqL._SL1500_.jpg"
+  };
+  const samsung = {
+    id: 2,
+    title: "Samsung Galaxy Note 8",
+    price: "69,999",
+    image:
+      "https://images.samsung.com/us/smartphones/galaxy-note20/pdp/gallery/canvas2/Black/Gallery-01-C2-Lockup-MysticBlack-1600x1200.jpg?$product-details-jpg"
+  };
+  const nokia = {
+    id: 3,
+    title: "Nokia 1100",
+    price: "2,999",
+    image: "https://m.media-amazon.com/images/I/41wGEmM0S4L.jpg"
+  };
+  const products = [iphone, samsung, nokia];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Product title={iphone.title} price={iphone.price} image={iphone.image}/>
+      <Product title={samsung.title} price={samsung.price} image={samsung.image}/>
+      <Product title={nokia.title} price={nokia.price} image={nokia.image}/> */}
+      {/* {products.map((product) => (
+        <Product
+          title={product.title}
+          image={product.image}
+          price={product.price}
+          key={product.title}
+        />
+      ))} */}
+      <Provider store={store}>
+        <CartContextProvider>
+          <Header
+            selectedCategory={selectedCategory}
+            setSelectedCategory={setSelectedCategory}
+          />
+          <ProductList selectedCategory={selectedCategory} />
+        </CartContextProvider>
+      </Provider>
     </div>
   );
 }
-
-export default App;
---
+++ src/store/index.ts
@@ -0,0 +1,11 @@
+import { createStore, combineReducers } from "redux";
+
+import cartReducer from "./cart";
+
+const reducer = combineReducers({
+  cart: cartReducer
+});
+
+const store = createStore(reducer);
+
+export default store;
